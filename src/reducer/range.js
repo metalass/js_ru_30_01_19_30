aa627b05205947f1eb45b54e53731cf23f9bdc01
Roman <kaboom.ua@gmail.com>
2017-02-15 23:01:05 +0200

    Update range.js

--- src/reducer/range.js
+++ src/reducer/range.js
@@ -1,5 +1,6 @@
 import {DATE_RANGE_CHANGE} from '../constants'
 
+//не дроби редюсеры слишком сильно: объедини селект и календарь в один
 export default (range = { from: null, to: null }, action) => {
 	const {type, payload} = action
 
