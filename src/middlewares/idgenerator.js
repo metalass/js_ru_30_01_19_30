a2ee137e15d374a788f64a8ae7b02ca52b0b7dc3
Roman <kaboom.ua@gmail.com>
2017-02-19 18:49:04 +0200

    Update idgenerator.js

--- src/middlewares/idgenerator.js
+++ src/middlewares/idgenerator.js
@@ -4,6 +4,7 @@ import {mapToArr} from '../utils'
 export default store => next => action => {
 	if (action.type && action.type == ADD_COMMENT) {
 		var currentStore = store.getState();
+		//достаточно плохая идея, никаких гарантий уникальности, в любой момент с сервера может прийти коммент с такой id
 		action.payload.commentid = mapToArr(currentStore.comments).reduce((max, comment) => {
 			if (max < +comment.id) {
 				max = +comment.id
